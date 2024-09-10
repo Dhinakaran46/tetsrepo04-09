2c40dd4d4d30e6d0b9719c1be34ce56137db302f
mukeshm_techcedence <mukeshm@techcedence.com>
2024-09-10 16:38:10 +0530

    up

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  //apiUrl: 'http://opensource.techcedence.net:3131',
-   apiUrl: 'http://localhost:3131',
+  apiUrl: 'http://opensource.techcedence.net/lcp-api',
+  // apiUrl: 'http://localhost:3131',
   apiAddress: '/api/',
   ENCRYPTION_KEY: 'FINfM8x6fs',
 };
