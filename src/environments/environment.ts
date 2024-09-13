b8b127903e4eab2150d7f7ea7959f6decb80b706
Karthick <karthickn@techcedence.com>
2024-09-13 18:56:04 +0530

    hooks function

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://opensource.techcedence.net/lcp-api',
+  apiUrl: 'https://opensource.techcedence.net/lcp-api',
   //apiUrl: 'http://localhost:3131',
   apiAddress: '/api/',
   ENCRYPTION_KEY: 'FINfM8x6fs',
