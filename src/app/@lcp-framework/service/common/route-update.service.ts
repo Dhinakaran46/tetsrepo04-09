b35525cddef240224426c1cdf5a2f3530f24f17f
logesht <logesht@techcedence.com>
2025-01-28 18:15:40 +0530

    10562/removed-dms-changes

--- src/app/@lcp-framework/service/common/route-update.service.ts
+++ src/app/@lcp-framework/service/common/route-update.service.ts
@@ -18,7 +18,6 @@ import { ImportMasterComponent } from '../../pages/import-master/import-master.c
 import { ImportTemplateComponent } from '../../pages/import-template/import-template.component';
 import { ExportTemplateComponent } from '../../pages/export-template/export-template.component';
 import { ImportJobDetailsComponent } from '../../pages/import-job-details/import-job-details.component';
-import { BarcodePrintingComponent } from '../../../@edms/pages/barcode-printing/barcode-printing.component';
 
 @Injectable({
   providedIn: 'root',
@@ -150,7 +149,6 @@ export class RouteUpdateService {
                 import_template_module: ImportTemplateComponent,
                 export_template_module: ExportTemplateComponent,
                 import_job_detail_module: ImportJobDetailsComponent,
-                barcode_print_module: BarcodePrintingComponent,
               };
 
               const route: Route = {
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://opensource.techcedence.net/lcp-edms-api',
-  //apiUrl: 'http://localhost:3737',
+  // apiUrl: 'https://opensource.techcedence.net/lcp-api',
+  apiUrl: 'http://localhost:3131',
   apiAddress: '/api/',
   ENCRYPTION_KEY: 'FINfM8x6fs',
-  WS_URL: 'ws://localhost:8091',
+  WS_URL: 'ws://localhost:8089',
 };
