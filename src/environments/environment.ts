7f1606a5b28863d6f2710272aed9d466bce0b929
mukeshm_techcedence <mukeshm@techcedence.com>
2025-04-02 10:40:41 +0530

    env status update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  // apiUrl: 'https://opensource.techcedence.net/lcp-api',
-  apiUrl: 'http://localhost:3131',
+  apiUrl: 'https://opensource.techcedence.net/lcp-api',
+  // apiUrl: 'http://localhost:3131',
   apiAddress: '/api/',
   ENCRYPTION_KEY: 'FINfM8x6fs',
   WS_URL: 'ws://localhost:8089',
