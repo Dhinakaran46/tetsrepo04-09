79d6e8e82275b16d1d3b1cf17a11a1acedbb8236
Rajkumar <rajkumars@techcedence.com>
2025-06-13 19:59:48 +0530

    payload encryption done

--- src/app/@lcp-framework/service/common/auth.service.ts
+++ src/app/@lcp-framework/service/common/auth.service.ts
@@ -5,12 +5,13 @@ import { commonConfig } from '../../config/common.config';
 import { environment } from '../../../../environments/environment';
 import { HttpService } from './http.service';
 import { LocalStorageService } from './local-storage.service';
+import { CryptoHttpService } from '../crypto-http.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private http: HttpClient, private localstore: LocalStorageService) {}
+  constructor(private http: HttpClient, private localstore: LocalStorageService, private cryptoHttp: CryptoHttpService) {}
 
   login(credentials: { email: string; password: string }): Observable<any> {
     return this.http.post<any>(`${environment.apiUrl}${environment.apiAddress}${commonConfig.API.login}`, credentials);
