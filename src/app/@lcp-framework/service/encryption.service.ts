62677728b1973fbe39009cc38063258abda6f63d
Rajkumar <rajkumars@techcedence.com>
2025-06-14 15:06:12 +0530

    up

--- src/app/@lcp-framework/service/encryption.service.ts
+++ src/app/@lcp-framework/service/encryption.service.ts
@@ -1,13 +1,16 @@
 // encryption.service.ts
 import { Injectable } from '@angular/core';
 import * as CryptoJS from 'crypto-js';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class EncryptionService {
-  private SECRET_KEY = CryptoJS.enc.Utf8.parse('12345678901234567890123456789012'); // 32-byte
-  private IV = CryptoJS.enc.Utf8.parse('1234567890123456'); // 16-byte
+  //private SECRET_KEY = CryptoJS.enc.Utf8.parse('12345678901234567890123456789012'); // 32-byte
+  //private IV = CryptoJS.enc.Utf8.parse('1234567890123456'); // 16-byte
+  private SECRET_KEY = CryptoJS.enc.Utf8.parse(environment.PAYLOAD_ENCRYPTION_KEY);
+  private IV = CryptoJS.enc.Utf8.parse(environment.PAYLOAD_ENCRYPTION_IV);
 
   encrypt(data: any): string {
     const encrypted = CryptoJS.AES.encrypt(JSON.stringify(data), this.SECRET_KEY, {
