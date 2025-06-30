091bb57b457184e3b986eb5eb36e4abe7f6f8a78
Rajkumar <rajkumars@techcedence.com>
2025-06-30 13:36:23 +0530

    Revert "30-06-2025/initial-setup"
    This reverts commit 3a8eef52bf751b25e6c74aaad3879f5545f115ea.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://opensource.techcedence.net/qacc-api',
-  // apiUrl: 'http://localhost:4001',
+  apiUrl: 'https://opensource.techcedence.net/lcp-api',
+  // apiUrl: 'http://localhost:3131',
   apiAddress: '/api/',
   ENCRYPTION_KEY: 'FINfM8x6fs',
-  WS_URL: 'ws://localhost:8101',
+  WS_URL: 'ws://localhost:8089',
   PAYLOAD_ENCRYPTION_KEY: '12345678901234567890123456789012', // 32 chars
   PAYLOAD_ENCRYPTION_IV: '1234567890123456', // 16 chars
 };
