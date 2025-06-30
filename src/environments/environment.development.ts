3a8eef52bf751b25e6c74aaad3879f5545f115ea
Rajkumar <rajkumars@techcedence.com>
2025-06-30 13:18:16 +0530

    30-06-2025/initial-setup

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: false,
-   apiUrl: 'https://lcp.techcedence.net/lcp-api',
-  //apiUrl: 'http://localhost:3131',
+   //apiUrl: 'https://lcp.techcedence.net/qacc-api',
+  apiUrl: 'http://localhost:4001',
   apiAddress: '/api/',
   ENCRYPTION_KEY: 'FINfM8x6fs',
-  WS_URL: 'ws://localhost:8090',
+  WS_URL: 'ws://localhost:8101',
   DB: 'pg', // 'sql' or 'pg'
   PAYLOAD_ENCRYPTION_KEY: '12345678901234567890123456789012', // 32 chars
   PAYLOAD_ENCRYPTION_IV: '1234567890123456', // 16 chars
