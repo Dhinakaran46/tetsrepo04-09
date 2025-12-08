e286f6483a71eac9b055e5de12eb568681ff9aa9
logesh-t-1892000 <logesh.t@datafortune.com>
2025-12-08 14:34:25 +0530

    pdf template shcema changes with prefix as pdf_ and language transaltion issue fix

--- src/app/@lcp-framework/service/common/local-storage-translate.service.ts
+++ src/app/@lcp-framework/service/common/local-storage-translate.service.ts
@@ -7,14 +7,10 @@ import { LocalStorageService } from './local-storage.service';
   providedIn: 'root',
 })
 export class LocalStorageTranslateLoader implements TranslateLoader {
-  //private localstore:any = LocalStorageService;
-  //constructor(private localstore: LocalStorageService) {}
+  constructor(private localstore: LocalStorageService) {}
   getTranslation(lang: string): Observable<any> {
-    const scope = (window.location.port || window.location.hostname + '' + window.location.pathname).replace('/', '-');
-    const key = `${scope}_lang_contents`;
+    const key = this.localstore.getScopedKey('lang_contents');
     const languageContent = JSON.parse(localStorage.getItem(key) || '{}');
-    //const languageContent = JSON.parse(localStorage.getItem('lang_contents') || '{}');
-    //const languageContent = JSON.parse(this.localstore.getData('lang_contents') || '{}');
     return of(languageContent);
   }
 }
