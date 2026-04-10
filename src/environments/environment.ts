cf65082921c309c597869f40e17f29dd5f38c917
MANOOWRANJITH.A.J <mentodesk@gmail.com>
2026-04-10 11:13:23 +0530

    undo env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
   // apiUrl: 'http://localhost:3131',
   apiAddress: '/api/',
   ENCRYPTION_KEY: 'FINfM8x6fs',
-  WS_URL: 'ws://localhost:8089',
+  WS_URL: 'ws://localhost:8090',
   PAYLOAD_ENCRYPTION_KEY: '12345678901234567890123456789012', // 32 chars
   PAYLOAD_ENCRYPTION_IV: '1234567890123456', // 16 chars
 };
